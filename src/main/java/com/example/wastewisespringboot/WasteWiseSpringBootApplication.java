582803351fee0098628c6ab555e4d8b0f4759918
BaekSeungJin_백승진 <tmdwls8387@ajou.ac.kr>
2024-06-14 14:06:08 +0900

    init

---
+++ src/main/java/com/example/wastewisespringboot/WasteWiseSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.wastewisespringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WasteWiseSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WasteWiseSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/wastewisespringboot/WasteWiseSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wastewisespringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WasteWiseSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
