fb3a7fdc157ae74b478cba2f71205d8d5abb2938
BaekSeungJin_백승진 <tmdwls8387@ajou.ac.kr>
2024-06-16 02:52:31 +0900

    ai인식 결과 송출까지 완료

--- src/main/java/com/example/wastewisespringboot/controller/ImageUploadController.java
+++ src/main/java/com/example/wastewisespringboot/controller/ImageUploadController.java
@@ -23,6 +23,7 @@ public class ImageUploadController {
 
     private String processedImageUrl;
     private String processedRegion;
+    private String processedClassification;
 
     @Autowired
     public ImageUploadController(ImageUploadService imageUploadService, RestTemplate restTemplate) {
@@ -34,7 +35,9 @@ public class ImageUploadController {
     public String upload(@RequestParam("image") MultipartFile image, @RequestParam("region") String region, Model model) throws IOException {
         // 이미지 업로드 로직
         String imageUrl = imageUploadService.upload(image);
+        System.out.println("이미지 업로드 완료: " + imageUrl);
 
+        System.out.println("FastAPI 서버로 데이터 전송 시작");
         // FastAPI 서버로 데이터 전송
         String fastApiUrl = "http://127.0.0.1:8000/imageregion";
         Map<String, String> requestData = new HashMap<>();
@@ -53,13 +56,15 @@ public class ImageUploadController {
         // FastAPI 서버로부터 받은 데이터를 처리
         processedImageUrl = data.get("imageUrl");
         processedRegion = data.get("region");
+        processedClassification = data.get("classification");
     }
 
     @GetMapping("/processed")
     public String getProcessedData(Model model) {
-        // 모델에 이미지 URL 및 지역 정보 추가
+        // 모델에 이미지 URL, 지역 정보 및 분류 결과 추가
         model.addAttribute("imageUrl", processedImageUrl);
         model.addAttribute("region", processedRegion);
+        model.addAttribute("classification", processedClassification);
         return "processed"; // 처리된 이미지를 표시할 페이지로 이동
     }
 }
